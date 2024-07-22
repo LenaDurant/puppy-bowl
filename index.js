70a5538c08533615c8fd43356067b73f3fc1a9f7
LenaDurant <durant.arlena@gmail.com>
2024-07-22 09:16:04 -0400

    finished sim for training

--- index.js
+++ index.js
@@ -13,17 +13,56 @@ form.addEventListener(`submit`, async (event) => {
   await getPuppyData(puppyName);
 });
 
-const getPuppyData = async (puppyName) => {
+window.onload = async () => {
+  state.puppyData = await getAllPuppies();
+  renderPuppyData(state.puppyData);
+}
+
+async function submitPlayer() {
+  let puppy = {
+    name: document.getElementById("playerName").value,
+    breed: document.getElementById("breed").value, 
+    status: document.getElementById("status").value,
+    imageUrl: document.getElementById("imageUrl").value,
+  };
+  let result = await addPuppy(puppy); 
+  state.puppyData = await getAllPuppies();
+  renderPuppyDetail(result);
+}
+
+// --- API Functions
+
+const getPuppyData = async (puppyId) => {
+  const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2406-ftb-et-web-ft/players/${puppyId}`); 
+  const responseJson = await response.json();
+  return responseJson.data.player;
+};
+
+const getAllPuppies = async () => {
   const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2406-ftb-et-web-ft/players`); 
   const responseJson = await response.json();
-  const puppyAPIData = responseJson.data.players;
-  state.puppyData = puppyAPIData;
-  renderPuppyData(); // 
+  return responseJson.data.players;
 };
+
+const addPuppy = async (puppy) => {
+  const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2406-ftb-et-web-ft/players`,
+    {
+      method: `POST`,
+      headers: {
+        'Content-Type': `application/json`,
+      },
+      body: JSON.stringify(puppy)
+    }
+  ); 
+  const responseJson = await response.json();
+  return responseJson.data.newPlayer;
+};
+
+// -----
 
 const renderPuppyData = () => {
   const main = document.querySelector(`main`);
-  main.innerHTML = ''; //
+  main.innerHTML = '<h2>Puppy Roster</h2>'; //
   const ol = document.createElement(`ol`);
   
   state.puppyData.forEach((puppy) => {
@@ -44,7 +83,7 @@ const renderPuppyDetail = (puppy) => {
   main.innerHTML = `
     <h2>${puppy.name}</h2>
     <h3>Breed: ${puppy.breed}</h3>
-    <h3>Status: ${puppy.status}</h3>
+    <h3>Status: ${puppy.status.charAt(0).toUpperCase() + puppy.status.slice(1)}</h3>
     <h3>Doggy id: ${puppy.id}</h3>
     <h3>Created at: ${puppy.createdAt}</h3>
     <h3>Updated at: ${puppy.updatedAt}</h3>
@@ -56,4 +95,5 @@ const renderPuppyDetail = (puppy) => {
 
   document.getElementById('backButton').addEventListener('click', renderPuppyData);
 };
+
 
